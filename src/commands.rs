68347f902054aea0f89eb691301e845f815e8883
Eric Raines <raineser@Erics-MacBook-Pro.local>
2024-12-12 18:16:33 -0600

    big clean

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, Subcommand};
-use crate::{day1, day3,  day4, day5, day6, day7, day8, day2, day9, day10, day11, day12};
+use crate::days::*;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
-use anyhow::Ok;
+use anyhow;
 use clap::{Parser};
-use commands::{Args, Commands, handle_command};
-
-
-mod day1;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day2;
-mod day9;
-mod day3;
-mod day10;
-mod day11;
-mod day12;
-
+use commands::{Args,handle_command};
+mod days;
 mod commands;
 
 fn main() -> anyhow::Result<()> {
-    
     let args = Args::parse();
-
     if let Some(command) = args.command {
         handle_command(command)?;
     }
