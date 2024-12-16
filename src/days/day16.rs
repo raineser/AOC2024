60c9506ea5bd44f9b00b02e83d148a74cadb9c3f
raineser <105230611+raineser@users.noreply.github.com>
2024-12-16 12:56:07 -0600

    day16 cleanup

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -19,7 +19,6 @@ impl Data {
         for line in reader.lines() {
             let line = line?;
             grid.push(line.chars().collect());
-
         }
 
         let mut start = (0,0);
@@ -37,7 +36,6 @@ impl Data {
     }
 }
 
-
 pub fn part_one() -> anyhow::Result<i64> {
     let mut data = Data::parse_input()?;
     let mut curr = data.start;
@@ -48,8 +46,6 @@ pub fn part_one() -> anyhow::Result<i64> {
     
     Ok(res)
 }
-
-
 
 fn dfs(grid: &Vec<Vec<char>>, curr: (i64,i64), dir: &[(i64,i64); 4], dir_index: usize, cache: &mut HashMap<(i64,i64,usize),i64>, score: i64, min_score: &mut i64 ) {
     if grid[curr.0 as usize][curr.1 as usize] == '#' {
@@ -66,18 +62,14 @@ fn dfs(grid: &Vec<Vec<char>>, curr: (i64,i64), dir: &[(i64,i64); 4], dir_index: 
             return
         }
     }
-
+    
     cache.insert((curr.0, curr.1, dir_index), score);
-
     let right = (dir_index + 1) % 4;
     let left = dir_index.checked_sub(1).unwrap_or(3);
     dfs(grid, (curr.0 + dir[dir_index].0, curr.1 + dir[dir_index].1), dir, dir_index, cache, score + 1, min_score);
     dfs(grid, (curr.0, curr.1), dir, right, cache, score + 1000, min_score);
     dfs(grid, (curr.0, curr.1), dir, left, cache, score + 1000, min_score);
 }
-
-
-
 
 pub fn part_two() -> anyhow::Result<i64> {
     let mut data = Data::parse_input()?;
@@ -91,7 +83,6 @@ pub fn part_two() -> anyhow::Result<i64> {
     
     while let Some((score, i, j, index, mut path)) = heap.pop() {
         path.push((i,j));
-        
         if data.grid[i as usize][j as usize] == 'E' {
             if score.0 <= res {
                 res = score.0;
@@ -111,13 +102,11 @@ pub fn part_two() -> anyhow::Result<i64> {
         }
         
         scores.insert((i,j,index), score.0);
-        
         let right = (index + 1) % 4;
         let left = index.checked_sub(1).unwrap_or(3);
         heap.push((Reverse(score.0 + 1), i + dir[index].0, j + dir[index].1, index, path.clone()));
         heap.push((Reverse(score.0 + 1001), i + dir[right].0, j + dir[right].1 , right, path.clone()));
         heap.push((Reverse(score.0 + 1001), i + dir[left].0, j + dir[left].1 , left, path));
     }
-
     Ok(min_path.len() as i64)
 }
